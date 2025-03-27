f6b5e2972cc3d714ba3549dc51577bc50213c215
Nout Vermeir <nout.vermeir@leerling.athenea.be>
2025-03-27 22:20:15 +0100

    klaar

--- oefening4.py
+++ oefening4.py
@@ -1,14 +1,10 @@
- target = int(input("input target"))
-nums = list(map(int, input("lijst").split(",")))
 def countTargetPairs(nums, target):
+    count = 0
+    n = len(nums)
     
-    TrPairs = 0
-    for i in range(len(nums)):
-        for j in range( i +1, len(nums)):
-            if i != j:
-                if nums[i] + nums[j] < target:
-                    TrPairs += 1
-    return TrPairs
-
-
-print(countTargetPairs(nums, target))
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] < target:
+                count += 1
+                
+    return count
